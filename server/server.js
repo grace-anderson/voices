8ebaef3cd1c43cd257e6b94b91a3f60d77041502
Grace <grace.anderson.au@gmail.com>
2022-08-02 17:52:36 +1000

    Updated server to get * as previous change errored with Heroku deploy

--- server/server.js
+++ server/server.js
@@ -31,11 +31,11 @@ const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
 
-  if (process.env.Node_ENV === "production") {
-    app.get("/", (req, res) => {
-      res.sendFile(path.join(__dirname, "../client/build/index.html"));
-    });
-  }
+	if (process.env.NODE_ENV === "production") {
+		app.get("*", (req, res) => {
+			res.sendFile(path.join(__dirname, "../client/build/index.html"));
+		});
+	}
 
   db.once("open", () => {
     app.listen(PORT, () => {
