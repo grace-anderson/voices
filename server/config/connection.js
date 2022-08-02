6ac160e41cf90d99ef2caf9068e8bfddd7681a5a
Grace <grace.anderson.au@gmail.com>
2022-08-02 19:33:19 +1000

    updated db details

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI,
-  // || 'mongodb://127.0.0.1:27017/voices',
-  { 
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/voices",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
