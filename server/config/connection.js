02986d582a304762a62858e6bf2bd792574bbe5f
Grace <grace.anderson.au@gmail.com>
2022-08-02 18:51:26 +1000

    removed mongodb local address

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/voices',
-  {
+  process.env.MONGODB_URI,
+  // || 'mongodb://127.0.0.1:27017/voices',
+  { 
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
